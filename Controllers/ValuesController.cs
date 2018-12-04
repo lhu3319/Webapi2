fa1069fcbb5a977f7e0587ae8499c205e595114f
Han <lhu3319@nate.com>
2018-12-04 15:27:15 +0900

    디비 세팅

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Webapi2.Modules;
+using System.Collections;
 
 namespace Webapi2.Controllers
 {
@@ -12,34 +15,23 @@ namespace Webapi2.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<ArrayList> Get()
         {
-            return new string[] { "value1", "value2" };
+            ArrayList list = new ArrayList();
+            MYsql my = new MYsql();
+            string sql = "select * from test;";
+            MySqlDataReader sdr = my.Reader(sql);
+            while (sdr.Read())
+            {
+                Hashtable ht = new Hashtable();
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    ht.Add(sdr.GetName(i), sdr.GetValue(i));
+                }
+                list.Add(ht);
+            }
+            return list;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
